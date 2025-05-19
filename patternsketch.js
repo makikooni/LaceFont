3403a4730a2cd0e63858cdf204f81e7d19b2b197
Weronika E. Falinska <w.e.falinska@gmail.com>
2025-05-20 00:04:01 +0100

    patterntools

--- patternsketch.js
+++ patternsketch.js
@@ -1,26 +1,75 @@
 let currentLayer = 0;
-let maxLayers = 10;
+let maxLayers = 30;
 let animate = true;
 let frameCounter = 0;
+let hasPlayedOnce = false;
+let clearBackground = false;
+
+let speedSlider;
+let cleanButton;
+let animCheckbox;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 360, 100, 100, 100);
   noFill();
   strokeWeight(1.5);
   angleMode(RADIANS);
+
+  // Speed Slider
+  createP('Animation Speed').position(420, 10).style('color', '#fff');
+  speedSlider = createSlider(1, 60, 10, 1);
+  speedSlider.position(400, 50);
+
+  // Clean Background Button
+  cleanButton = createButton('Clean Background');
+  cleanButton.position(250, 50);
+  cleanButton.mousePressed(() => {
+    clearBackground = true;
+  });
+
+  // Animate Checkbox
+createP('Constant?').position(560, 10).style('color', '#fff');
+  animCheckbox = createCheckbox("", false); // Start unchecked
+  animCheckbox.position(580, 50);
+  animCheckbox.style('color', '#fff');
+  animCheckbox.changed(() => {
+    if (animCheckbox.checked()) {
+      currentLayer = 0;
+      frameCounter = 0;
+      animate = true;
+      hasPlayedOnce = false;
+    }
+  });
 }
 
 function draw() {
-  background(255, 12); // Soft trailing effect
+  // Background fading
+  if (clearBackground) {
+    background(10, 80, 20, 100); // Full wipe
+    clearBackground = false;
+  } else {
+    background(10, 80, 20, 9); // Trail mode
+  }
 
+  // Glowing trails
+  blendMode(ADD);
   for (let i = 0; i < currentLayer; i++) {
     drawRadialPattern(i);
   }
+  blendMode(BLEND);
 
-  if (animate && currentLayer < maxLayers) {
+  // One-time growing animation
+  if (animate && !hasPlayedOnce) {
     frameCounter++;
-    if (frameCounter % 10 === 0) {
+    if (frameCounter % speedSlider.value() === 0) {
       currentLayer++;
+
+      if (currentLayer >= maxLayers) {
+        animate = false;
+        hasPlayedOnce = true;
+        animCheckbox.checked(false); // Visually uncheck the box
+      }
     }
   }
 }
@@ -29,9 +78,9 @@ function drawRadialPattern(layer) {
   push();
   translate(width / 2, height / 2);
 
-  let arms = int(map(mouseX, 0, width, 4, 20));
+  let arms = int(map(mouseX, 0, width, 4, 100));
   let radius = layer * map(mouseY, 0, height, 20, 100);
-  let r = map(mouseX + mouseY, 0, width + height, 20, 80);
+  let r = map(mouseX + mouseY, 40, width + height, 20, 80);
 
   for (let a = 0; a < arms; a++) {
     let angle = TWO_PI / arms * a;
